bf67c07097a7662c933bd98c7a9ee0b4178a128a
Connor Goodman <connorgoodman@msn.com>
2023-06-19 22:23:19 -0700

    Added additional styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,131 +1,120 @@
-import { AppBar, Button, Toolbar, Typography, Box, InputBase, MenuItem} from "@mui/material";
+import { AppBar, Button, Toolbar, Typography, Box, InputBase, MenuItem } from "@mui/material";
 import { styled, alpha } from '@mui/material/styles';
 import { useNavigate } from "react-router-dom";
-//import RiddleLockerLogo from '../logo.svg'
 import { useState } from "react";
 
 const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '15ch',
-        '&:focus': {
-          width: '20ch',
-        },
+      width: '15ch',
+      '&:focus': {
+        width: '20ch',
       },
     },
-  }));
+  },
+}));
 
-function NavBar(){
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  fontWeight: 'bold',
+}));
 
-    const [search, setSearch] = useState('');
-    const navigate = useNavigate();
+const NavBar = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
 
-    function Submit(e) {
-        e.preventDefault();
-        if (!ValidationSearch()) {
-            return;
-        }
-
-        console.log(search);
-        navigate('/viewriddle?riddle=' + search);
+  function Submit(e) {
+    e.preventDefault();
+    if (!ValidationSearch()) {
+      return;
     }
 
-    function ValidationSearch() {
-        if (search === '' || search === null || search === undefined || search.length < 1) {
-            return false;
-        }
-        return true;
+    console.log(search);
+    navigate('/viewriddle?locker=' + search);
+  }
+
+  function ValidationSearch() {
+    if (search === '' || search === null || search === undefined || search.length < 1) {
+      return false;
     }
+    return true;
+  }
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              {/* <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton> */}
-              <MenuItem
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                onClick={() => navigate('/')}
-              >
-                <Typography style={{fontWeight:"bold"}}>Riddle Locker</Typography>
-              </MenuItem>
-              <MenuItem
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ flexGrow: 1, display: { xs: 1} }}
-                onClick={() => navigate('/createriddle')}
-              >
-                <Typography style={{textAlign:"center"}}>Create a Riddle</Typography>
-                
-              </MenuItem>
-              <MenuItem
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ flexGrow: 1, display: { xs: 1} }}
-                onClick={() => navigate('/createriddle')}
-              >
-                <Typography style={{textAlign:"center"}}>About</Typography>
-                
-              </MenuItem>
-              <Box component="form" onSubmit={Submit}>
-                <Search>
-                    <SearchIconWrapper >
-                    </SearchIconWrapper>
-                    <StyledInputBase
-                    placeholder="Find a locker..."
-                    inputProps={{ 'aria-label': 'search' }}
-                    onChange={(e) => setSearch(e.target.value)}
-                    />
-                </Search>
-              </Box>
-              <Typography><Button>Button</Button></Typography>
-              </Toolbar>
-          </AppBar>
-        </Box>
-      );
-}
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#000724' }}>
+        <Toolbar>
+          <MenuItem
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            onClick={() => navigate('/')}
+          >
+            <StyledTypography>Riddle Locker</StyledTypography>
+          </MenuItem>
+          <MenuItem
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'block', sm: 'none' } }}
+            onClick={() => navigate('/createriddle')}
+          >
+            <Typography style={{ textAlign: "center" }}>Create a Riddle</Typography>
+          </MenuItem>
+          <MenuItem
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'block', sm: 'none' } }}
+            onClick={() => navigate('/about')}
+          >
+            <Typography style={{ textAlign: "center" }}>About</Typography>
+          </MenuItem>
+          <Box component="form" onSubmit={Submit}>
+            <Search>
+              <SearchIconWrapper></SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Find a locker..."
+                inputProps={{ 'aria-label': 'search' }}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </Search>
+          </Box>
+          <Button>Button</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/ViewRiddle.js
+++ src/components/ViewRiddle.js
@@ -1,136 +1,196 @@
 import React, { useState, useEffect } from 'react';
-import { FormControl, Button, Input, FormHelperText} from '@mui/material';
-import openlock from '../images/unlocked.jpg'
-import closedlock from '../images/locked.jpg'
+import { FormControl, Button, Input, FormHelperText, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import openlock from '../images/unlocked.jpg';
+import closedlock from '../images/locked.jpg';
 
-function ViewRiddle() {
-    const [data, setData] = useState();
-    const [answer, setAnswer] = useState();
-    const [locker, setLocker] = useState();
-    const [secret, setSecret] = useState();
-    const [showIncorrect, setShowIncorrect] = useState(false);
-    const [showSecret, setShowSecret] = useState(false);
+const PageContainer = styled('div')({
+  backgroundColor: '#f2f2f2',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  minHeight: '100vh',
+  fontFamily: 'Avenir, sans-serif',
+});
 
-    const [isError, setIsError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('')
+const SectionContainer = styled('div')({
+  backgroundColor: '#000724',
+  borderRadius: '8px',
+  padding: '2rem',
+  textAlign: 'center',
+  boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.4)',
+});
 
-    const [isLoading, setIsLoading] = useState(true);
+const EngravedText = styled('h1')({
+  fontFamily: 'Avenir, sans-serif',
+  fontSize: '2.5rem',
+  fontWeight: 'bold',
+  color: '#fff',
+  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
+  letterSpacing: '2px',
+});
 
-    useEffect(() => {
-      (async function () {
-        const queryParams = new URLSearchParams(window.location.search);
-    
-        setLocker(queryParams.get('locker'));
-        try {
-          const response = await fetch(`api/GetLocker?` + new URLSearchParams({
-            locker: queryParams.get('locker')
-          }));
-          
-          if (!response.ok) {
-            if (response.status === 404) {
-              throw new Error('Locker not found');
-            } else {
-              throw new Error("Can't connect to server");
-            }
+const LineDivider = styled('hr')({
+  border: 0,
+  borderTop: '1px solid #777777',
+  margin: '2rem 0',
+});
+
+const ImageContainer = styled('div')({
+  marginTop: '2rem',
+});
+
+const Image = styled('img')({
+  width: '200px',
+});
+
+const ViewRiddle = () => {
+  const [data, setData] = useState();
+  const [answer, setAnswer] = useState();
+  const [locker, setLocker] = useState();
+  const [secret, setSecret] = useState();
+  const [showIncorrect, setShowIncorrect] = useState(false);
+  const [showSecret, setShowSecret] = useState(false);
+
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async function () {
+      const queryParams = new URLSearchParams(window.location.search);
+
+      setLocker(queryParams.get('locker'));
+      try {
+        const response = await fetch(`api/GetLocker?` + new URLSearchParams({
+          locker: queryParams.get('locker')
+        }));
+
+        if (!response.ok) {
+          if (response.status === 404) {
+            throw new Error('Locker not found');
+          } else {
+            throw new Error("Can't connect to server");
           }
-          
-          const text = await response.json();
-          setData(text);
-          console.log(text);
-        } catch (error) {
-          setIsLoading(false);
-          setIsError(true);
-          setErrorMessage(error.message);
-          console.log('Error:', error);
         }
-      })();
-    }, []);
 
-    async function Submit() {
-        
-      fetch(`api/TryGetSecret/?` + 
-          new URLSearchParams({
-          locker: locker,
-          answer: answer
-          }),
-          {method: "POST"})
-          .then((response) => response.json()).then((r) => {
-              console.log('Response:', r)
-              if (r.isCorrect) {
-                setSecret(r.Secret);
-                setShowIncorrect(false)
-                setShowSecret(true)
-              }
-              else {
-                setShowIncorrect(true)
-                setShowSecret(false)
-              }
-              
-           })
-          .catch((error) => {
-              console.log('error: ' + error);
-              return;
-          });
+        const text = await response.json();
+        setData(text);
+        console.log(text);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+        setErrorMessage(error.message);
+        console.log('Error:', error);
+      }
+    })();
+  }, []);
 
+  const handleSubmit = () => {
+    fetch(`api/TryGetSecret/?` +
+      new URLSearchParams({
+        locker: locker,
+        answer: answer
+      }),
+      { method: "POST" })
+      .then((response) => response.json())
+      .then((r) => {
+        console.log('Response:', r);
+        if (r.isCorrect) {
+          setSecret(r.Secret);
+          setShowIncorrect(false);
+          setShowSecret(true);
+        } else {
+          setShowIncorrect(true);
+          setShowSecret(false);
+        }
+      })
+      .catch((error) => {
+        console.log('error: ' + error);
+        return;
+      });
   };
-    
-    return (
 
-        <div>
-          {data && 
-          (<div>
-            <h1>Locker: {locker}</h1>
-            <h2>The Riddle Is:</h2>
-            <h2>{data.Riddle}</h2>
-            <br/>
+  return (
+    <PageContainer>
+      <SectionContainer>
+        {data && (
+          <div>
+            <EngravedText>{locker}</EngravedText>
+            <LineDivider />
+            <Typography variant="h5" component="h2" gutterBottom style={{ color: '#fff' }}>
+              The Riddle Is:
+            </Typography>
+            <Typography variant="h5" component="h2" gutterBottom style={{ color: '#fff' }}>
+              {data.Riddle}
+            </Typography>
+            <br />
             <FormControl>
-                <Input id="passcode-input" aria-describedby="passcode-helper-text" onChange={ (e) => setAnswer(e.target.value) }/>
-                <FormHelperText id="passcode-helper-text">Enter your guess</FormHelperText>
+              <Input
+                id="passcode-input"
+                aria-describedby="passcode-helper-text"
+                onChange={(e) => setAnswer(e.target.value)}
+              />
+              <FormHelperText id="passcode-helper-text" style={{ color: '#fff' }}>
+                Enter your guess
+              </FormHelperText>
             </FormControl>
 
-            {!showSecret && 
-            (<FormControl>
-                <Button onClick={(e) => Submit() }>Submit</Button>
-            </FormControl>)}
-            
+            {!showSecret && (
+              <FormControl>
+                <Button onClick={handleSubmit}>Submit</Button>
+              </FormControl>
+            )}
+
             {!showSecret && (
               <div>
-                <br/>
-                <img src={closedlock} alt="locked padlock"></img>
-                <br/>
+                <LineDivider />
+                <ImageContainer>
+                  <Image src={closedlock} alt="locked padlock" />
+                </ImageContainer>
+              </div>
+            )}
+
+            {showIncorrect && (
+              <Typography variant="h6" component="div" color="error">Incorrect! Try again!</Typography>
+            )}
+
+            {showSecret && (
+              <div>
+                <LineDivider />
+                <ImageContainer>
+                  <Image src={openlock} alt="unlocked padlock" />
+                </ImageContainer>
               </div>
             )}
 
             {showSecret && (
               <div>
-                <br/>
-                <img src={openlock} alt="unlocked padlock"></img>
-                <br/>
+                <Typography variant="h5" component="h2" gutterBottom style={{ color: '#fff' }}>
+                  Correct! Unveiling the secret:
+                </Typography>
+                <br />
+                <Typography variant="h6" component="div" style={{ color: '#fff' }}>
+                  {secret}
+                </Typography>
               </div>
             )}
+          </div>
+        )}
+        {isLoading && (
+          <Typography variant="h5" component="h2" style={{ color: '#fff' }}>
+            Loading Riddle
+          </Typography>
+        )}
+        {isError && (
+          <Typography variant="h5" component="h2" color="error">
+            {errorMessage}
+          </Typography>
+        )}
+      </SectionContainer>
+    </PageContainer>
+  );
+};
 
-            {showIncorrect && (<div>
-              Incorrect! Try again!
-            </div>)}
-
-            {showSecret && (<div>
-              <h2>Correct! Unveiling the secret:</h2>
-              <br/>
-              <span>{secret}</span>
-            </div>)}
-
-            </div>)
-          }
-          {
-            isLoading && (<h2>Loading Riddle</h2>)
-          }
-          {
-            isError && (<h2>{errorMessage}</h2>)
-          }
-        </div>
-    )
-    
-
-    
-}
 export default ViewRiddle;
